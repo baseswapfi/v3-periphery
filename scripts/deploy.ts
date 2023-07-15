12271f7723986c6d49d69b1c42d9535b06a7177a
0xshoegazer <0xshoegazer@gmail.com>
2023-07-15 10:22:44 -0400

    NFT descriptor deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,23 @@
+import { deployTokenPositionDescriptor } from './utils/contract.utils';
+
 const hardhat = require('hardhat');
 
 // BASE
 const FACTORY = '0x1b527E8690F0c57ea546e72Fb0c66a312118A941';
 const WETH = '0x4200000000000000000000000000000000000006';
 
-const SWAP_ROUTER = '0xff139bEa1E15Eff04e8Fa3ac8dd29401C74F323D'; // block 1251774
-const QUOTER_V2 = '0x6d561f79A1C302fD18CFd87aC7b4571Ec84Ca5A7';
+// ethers.utils.formatBytes32String("ETH")
+const nativeCurrencyLabelBytes =
+  '0x4554480000000000000000000000000000000000000000000000000000000000';
+const NFTDescriptor_BASE = '0xE4e56751d115773b61a8DEED0737710139dB55F7';
 
 async function main() {
-  const SwapRouter = await hardhat.ethers.getContractFactory('SwapRouter');
-  const router = await SwapRouter.deploy(FACTORY, WETH);
-  await router.deployed();
-  console.log('SwapRouter at: ' + router.address);
-
-  const QuoterV2 = await hardhat.ethers.getContractFactory('QuoterV2');
-  const quote = await QuoterV2.deploy(FACTORY, WETH);
-  await quote.deployed();
-  console.log('QuoterV2 at: ' + quote.address);
+  try {
+    // await deployTokenPositionDescriptor(WETH, nativeCurrencyLabelBytes, NFTDescriptor_BASE);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 }
 
-main().catch(console.log);
+main();
---
+++ scripts/utils/contract.utils.ts
@@ -0,0 +1,46 @@
+import { ethers } from 'hardhat';
+
+export async function deployRouter(factory: string, WETH: string) {
+  const SwapRouter = await ethers.getContractFactory('SwapRouter');
+  const router = await SwapRouter.deploy(factory, WETH);
+  await router.deployed();
+  console.log('SwapRouter at: ' + router.address);
+
+  return router;
+}
+
+export async function deployQuoterV2(factory: string, WETH: string) {
+  const QuoterV2 = await ethers.getContractFactory('QuoterV2');
+  const quote = await QuoterV2.deploy(factory, WETH);
+  await quote.deployed();
+  console.log('QuoterV2 at: ' + quote.address);
+
+  return quote;
+}
+
+export async function deployTokenPositionDescriptor(
+  WETH: string,
+  nativeCurrencyLabelBytes: string,
+  NFTDescriptor?: string
+) {
+  // Link the library
+  if (!NFTDescriptor) {
+    const descriptor = await ethers.getContractFactory('NFTDescriptor');
+    const instance = await descriptor.deploy();
+    await instance.deployed();
+    console.log('NFTDescriptor library delpoyed at: ' + instance.address);
+
+    NFTDescriptor = instance.address;
+  }
+
+  const Factory = await ethers.getContractFactory('NonfungibleTokenPositionDescriptor', {
+    libraries: {
+      NFTDescriptor,
+    },
+  });
+  const instance = await Factory.deploy(WETH, nativeCurrencyLabelBytes);
+  await instance.deployed();
+  console.log('NonfungibleTokenPositionDescriptor deployed at: ' + instance.address);
+
+  return instance;
+}
