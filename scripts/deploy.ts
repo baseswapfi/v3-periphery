581cc4f4568cc45407c370e47555dfafb18ef747
0xshoegazer <0xshoegazer@gmail.com>
2023-09-01 19:21:52 -0400

    stash

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,3 @@
-import { ethers } from 'hardhat';
 import {
   deployMigrator,
   deployPositionManager,
@@ -16,21 +15,21 @@ const nativeCurrencyLabelBytes =
   '0x4554480000000000000000000000000000000000000000000000000000000000';
 
 const WETH = '0x4200000000000000000000000000000000000006';
-const FACTORY = '0x60a610de5CB6aa716ba79cEa926cbcc7bFFc43e7';
+const FACTORY = '0x38015D05f4fEC8AFe15D7cc0386a126574e8077B';
 
-const NFTDescriptorLibrary = '0xC81f114F8d7B12363211F64F649D2376d654499D';
-const NonfungibleTokenPositionDescriptor = '0x91b982439132987b6B656eC4B3d0EE4F3B5cb26B';
-const NonfungiblePositionManager = '0xb0bbb78d7dDD1e8c7c6d0f9B49ED3478B4bdE455';
-const ROUTER = '0x14963869592E41a2ecD7524bD213cFC3044AdF75';
+const NFTDescriptorLibrary = '0x706136AF1953291DcBd2311b7f8e4c778DA9D51b';
+const NonfungibleTokenPositionDescriptor = '0x38D7FDfBDb6aa83d78e200bEAf5B587123ae2a13';
+const NonfungiblePositionManager = '0x2C73D2d528ebC6d19E4e0177080851e709E18BF5';
+const ROUTER = '';
 
 async function main() {
   try {
-    const signer = (await ethers.getSigners())[0];
-    // await deployRouter(FACTORY, WETH, signer);
+    const signer = (await hardhat.ethers.getSigners())[0];
+    await deployRouter(FACTORY, WETH, signer);
     // await deployQuoter(FACTORY, WETH);
     // await deployQuoterV2(FACTORY, WETH);
     // await deployTickLens();
-    // await deployTokenPositionDescriptor(WETH, nativeCurrencyLabelBytes, '');
+    // await deployTokenPositionDescriptor(WETH, nativeCurrencyLabelBytes, NFTDescriptorLibrary);
     // await deployPositionManager(FACTORY, WETH, NonfungibleTokenPositionDescriptor);
     // await deployMigrator(FACTORY, WETH, NonfungiblePositionManager, signer);
   } catch (error) {
